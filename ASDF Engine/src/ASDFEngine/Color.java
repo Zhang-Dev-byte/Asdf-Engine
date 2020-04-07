415e26ae067bcf99104fbb904d22cdb38acd9766
Zhang-Dev-byte <63158613+Zhang-Dev-byte@users.noreply.github.com>
2020-04-07 14:44:32 -0700

    Added Graphics Support

---
+++ ASDF Engine/src/ASDFEngine/Color.java
@@ -0,0 +1,15 @@
+package ASDFEngine;
+
+public class Color {
+	public float R;
+	public float G;
+	public float B;
+	public float A;
+	
+	public Color(float _R,float _G,float _B,float _A) {
+		R = _R;
+		G = _G;
+		B = _B;
+		A = _A;
+	}
+}
